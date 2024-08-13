8bae2a1525b751c900fb72f9e70bf4f240da40e3
Joe <jomonjoshygeorge@gmail.com>
2024-08-13 20:34:45 +0530

    removed api key

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,11 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:google_place_picker/google_place_picker.dart';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'dart:io' show Platform;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,67 +44,34 @@ class GooglePlacePickerExample extends StatefulWidget {
 class _GooglePlacePickerExampleState extends State<GooglePlacePickerExample> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: MapSample(),
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class MapSample extends StatefulWidget {
-  const MapSample({super.key});
-
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  final Completer<GoogleMapController> _controller =
-  Completer<GoogleMapController>();
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+      body: Center(
+        child: TextButton(
+          child: Text("Pick Delivery location"),
+          onPressed: () {
+            showPlacePicker();
+          },
+        ),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  void showPlacePicker() async {
+    LocationResult? result = await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => PlacePicker(
+          Platform.isAndroid
+              ? FlutterConfig.get('GOOGLE_MAPS_API_KEY_ANDROID')
+              : FlutterConfig.get('GOOGLE_MAPS_API_KEY_IOS'),
+          displayLocation: const LatLng(
+            29.378586,
+            47.990341,
+          ),
+        ),
+      ),
+    );
+
+    // Handle the result in your way
+    print(result);
   }
 }
